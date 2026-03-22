9b7f98d6beab0d59f2da9a7b37a10678d6c11e4a
William <quagsire089@gmail.com>
2026-03-21 23:29:51 -0600

    Added Shrink ability to AI

---
+++ Assets/Scripts/Gameplay/AI/BattlePhaseAI/ShrinkAIAbility.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShrinkAIAbility : MonoBehaviour
+{
+    private AbilityCard _shrinkAbility;
+
+    public ShrinkAIAbility(AbilityCard ability)
+    {
+        _shrinkAbility = ability;
+    }
+
+    public bool CanExecute()
+    {
+        return CombatManager.Instance.GetAIStamina() >= _shrinkAbility.staminaCost;
+    }
+
+    public IEnumerator Execute()
+    {
+        var enemies = CubeSpawner.Instance.ReturnPlayerCubes();
+        var myCubes = CubeSpawner.Instance.ReturnAICubes();
+
+        if (enemies.Count == 0 || myCubes.Count == 0)
+            yield break;
+
+        var user = myCubes[Random.Range(0, myCubes.Count)];
+        var target = enemies[Random.Range(0, enemies.Count)];
+
+        CombatManager.Instance.QueueAbility(user, target, _shrinkAbility);
+
+        yield return new WaitForSeconds(0.2f);
+    }
+}

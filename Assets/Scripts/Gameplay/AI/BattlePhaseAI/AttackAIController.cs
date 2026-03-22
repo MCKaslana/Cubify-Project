5d8d690b05c095f3734df53471f12f057fce1682
William <quagsire089@gmail.com>
2026-03-21 23:22:46 -0600

    Added fields for AI abilities

--- Assets/Scripts/Gameplay/AI/BattlePhaseAI/AttackAIController.cs
+++ Assets/Scripts/Gameplay/AI/BattlePhaseAI/AttackAIController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AttackAIController : MonoBehaviour
 {
+    [SerializeField] private AbilityCard _attackAbility;
+    [SerializeField] private AbilityCard _swapAbility;
+    [SerializeField] private AbilityCard _redirectAbility;
+    [SerializeField] private AbilityCard _interruptAbility;
+    [SerializeField] private AbilityCard _shrinkAbility;
+
     private List<IAIAttackAction> _actions = new();
+
+    private void Awake()
+    {
+        Initialize
+            (_attackAbility, _swapAbility, _redirectAbility, _interruptAbility, _shrinkAbility);
+    }
 
     public void Initialize(
         AbilityCard attack,
         AbilityCard swap,
         AbilityCard redirect,
-        AbilityCard interrupt)
+        AbilityCard interrupt,
+        AbilityCard shrink)
     {
         _actions.Clear();
+
+        _actions.Add(new AttackAction(attack));
+
     }
 
     public IEnumerator ExecuteTurn(int maxActions)
